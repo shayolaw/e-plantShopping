e89a5fd4ec39369fde2e95307f9e89c2fbbe864c
sayo law <shayolaw@gmail.com>
2024-11-21 17:59:54 -0500

    final commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -5,6 +5,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
+    const cart = useSelector(state=>state.cart.items)
+   
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({})
     const dispatch = useDispatch();
@@ -236,6 +238,7 @@ function ProductList() {
     fontSize: '30px',
     textDecoration: 'none',
    }
+   const quantity = cart.length;
    const handleCartClick = (e) => {
 
     e.preventDefault();
@@ -280,16 +283,27 @@ const handlePlantsClick = (e) => {
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
                 <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>
+                <p style={{
+                    position:"relative",
+                    left:"40px",
+                    top:"20px",
+                    fontSize:"0.4em"
+                }}>
+                    {quantity}
+                </p>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
                         <rect width="156" height="156" fill="none">
                         </rect>
+                       
                         <circle cx="80" cy="216" r="12">
+                        
                         </circle>
                         <circle cx="184" cy="216" r="12">
                         </circle>
                     <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute">
 
                     </path>
+                   
                     </svg></h1></a></div>
             </div>
         </div>

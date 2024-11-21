2b1ddc97543dc59972a5055b7e5d599107a2195f
sayo law <shayolaw@gmail.com>
2024-11-21 15:07:49 -0500

    added task 2

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {name, image, cost} = action.payload;
+        const plant = state.items.find(item=> item.name === name);
+        if(plant){
+            plant.quantity++;
+        }
+        else{
+            state.items.push({name, image, cost, quantity:1})
+        }
     },
     removeItem: (state, action) => {
+        const {name,image,cost} = action.payload;
+        state.items = state.items.filter(item => item.name !== name)
+
     },
     updateQuantity: (state, action) => {
-
+    const {name,quantity} = action.payload;
+    const item = state.items.find(item=> item.name === name)
+    item.quantity = quantity;
     
     },
   },
